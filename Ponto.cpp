3bf82cd9526b440cef251b56b0296396893132cc
davi steindorfer <davibs42@gmail.com>
2022-01-04 12:38:27 -0300

    ponto incompleto

---
+++ Ponto.cpp
@@ -0,0 +1,10 @@
+#include "Ponto.hpp";
+Ponto::Ponto(double x1, double y1, double z1) {
+    x = x1;
+    y = y1;
+    z = z1;
+}
+void Ponto::set_x(double x2) {
+    x = x2;
+}
+
---
+++ Ponto.hpp
@@ -0,0 +1,18 @@
+#ifndef Ponto_HPP;
+#define Ponto_HPP;
+
+
+class Ponto{
+    private:
+        double x,y,z;
+    Ponto(double, double, double);
+    void set_x(double);
+    void set_y(double);
+    void set_z(double);
+    double get_x();
+    double get_y();
+    double get_z();
+
+};
+
+#endif
